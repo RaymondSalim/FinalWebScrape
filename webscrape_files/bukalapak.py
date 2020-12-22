f2429a10cef2c0179316fb724700bb65b0e528dc
Raymond Salim <raymond.salim9@gmail.com>
2020-12-22 12:34:37 +0700

    Replaced char \ua789 with -

--- webscrape_files/bukalapak.py
+++ webscrape_files/bukalapak.py
@@ -311,13 +311,13 @@ class Bukalapak:
             return self.NEXT_PAGE_DEAD
 
     def handle_data(self):
-        end_time = str(datetime.now() - self.start_time).replace(':', '꞉')
+        end_time = str(datetime.now() - self.start_time).replace(':', '-')
         print("Time taken: " + end_time, flush=True)
 
         if self.args['command'] == "scrape":
             if self.args['filename'] == '':
                 # Filename argument is not specified, so filename will be generated
-                self.args['filename'] = f"{self.args['query']}_{self.ID}_{str(datetime.now()).replace(':', '꞉')}"
+                self.args['filename'] = f"{self.args['query']}_{self.ID}_{str(datetime.now()).replace(':', '-')}"
 
             else:
                 self.args['filename'] = self.args['filename']
